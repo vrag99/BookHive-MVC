03cbacf4051706c8eb46cd8d860529e809e34c1c
vrag99 <garv.codes@gmail.com>
2023-08-07 23:33:31 +0530

    clean userDashboard controller

--- pkg/controller/userDashboard.go
+++ pkg/controller/userDashboard.go
@@ -7,6 +7,7 @@ import (
 	"BookHive/pkg/views"
 	"fmt"
 	"net/http"
+	"reflect"
 
 	"github.com/gorilla/mux"
 )
@@ -23,120 +24,11 @@ func UserViews(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	db, _ := models.Connection()
-	defer db.Close()
-
-	// w.Write([]byte("ye lo username: " + claims["username"].(string)))
-
-	if viewMode == "requested" {
-		rows := utils.ExecSql(db, `
-			select b.*
-			from books b
-			inner join requests r on b.id = r.book_id
-			where r.status = 'request-issue' and r.user_id = ? and b.available_qty>=1;
-		`, claims["id"])
-		defer rows.Close()
-
-		var fetchBooks []types.Book
-		for rows.Next() {
-			var book types.Book
-			err := rows.Scan(&book.Id, &book.Name, &book.Qty, &book.AvailableQty)
-			if err != nil {
-				fmt.Println("Error fetching books for userViews")
-				panic(err)
-			}
-			fetchBooks = append(fetchBooks, book)
-		}
-
-		data := types.UserViewData{
-			State: viewMode,
-			Books: fetchBooks,
-		}
-		t := views.UserDashboard()
-		t.Execute(w, data)
-
-	} else if viewMode == "issued" {
-		rows := utils.ExecSql(db, `
-			select b.*
-			from books b
-			inner join requests r on b.id = r.book_id
-			where r.status = 'issued' and r.user_id = ? and b.available_qty>=1;
-		`, claims["id"])
-		defer rows.Close()
-
-		var fetchBooks []types.Book
-		for rows.Next() {
-			var book types.Book
-			err := rows.Scan(&book.Id, &book.Name, &book.Qty, &book.AvailableQty)
-			if err != nil {
-				fmt.Println("Error fetching books for userViews")
-				panic(err)
-			}
-			fetchBooks = append(fetchBooks, book)
-		}
-
-		data := types.UserViewData{
-			State: viewMode,
-			Books: fetchBooks,
-		}
-		t := views.UserDashboard()
-		t.Execute(w, data)
-
-	} else if viewMode == "to-be-returned" {
-		rows := utils.ExecSql(db, `
-			select b.*
-			from books b
-			inner join requests r on b.id = r.book_id
-			where r.status = 'issued' and r.user_id = ? and b.available_qty>=1;
-		`, claims["id"])
-		defer rows.Close()
-
-		var fetchBooks []types.Book
-		for rows.Next() {
-			var book types.Book
-			err := rows.Scan(&book.Id, &book.Name, &book.Qty, &book.AvailableQty)
-			if err != nil {
-				fmt.Println("Error fetching books for userViews")
-				panic(err)
-			}
-			fetchBooks = append(fetchBooks, book)
-		}
-
-		data := types.UserViewData{
-			State: viewMode,
-			Books: fetchBooks,
-		}
-		t := views.UserDashboard()
-		t.Execute(w, data)
-
-	} else {
-		viewMode := "available"
-		rows := utils.ExecSql(db, `
-			select b.*
-			from books b
-			left join requests r on b.id = r.book_id
-			and r.user_id = ?
-			where r.id is NULL and available_qty>=1;
-		`, claims["id"])
-		defer rows.Close()
-
-		var fetchBooks []types.Book
-		for rows.Next() {
-			var book types.Book
-			err := rows.Scan(&book.Id, &book.Name, &book.Qty, &book.AvailableQty)
-			if err != nil {
-				fmt.Println("Error fetching books for userViews")
-				panic(err)
-			}
-			fetchBooks = append(fetchBooks, book)
-		}
-
-		data := types.UserViewData{
-			Username: claims["username"].(string),
-			State:    viewMode,
-			Books:    fetchBooks,
-		}
-
+	data := models.GetBooksOnViewMode(viewMode, claims)
+	if reflect.DeepEqual(data, types.UserViewData{}){
+		// If got no data
+		fmt.Fprintf(w, "No data. Might be due to an invalid viewMode or error in fetching books.")
+	} else{
 		t := views.UserDashboard()
 		t.Execute(w, data)
 	}
