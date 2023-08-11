a5f13ddedf82a90531479683492d4eeecd67e1d1
vrag99 <garv.codes@gmail.com>
2023-08-12 02:18:51 +0530

    implement navbars for switching views

--- static/scripts/userDashboard.js
+++
@@ -1,8 +0,0 @@
-var viewMode = document.getElementById("viewMode");
-viewMode.addEventListener("change", () => {
-  var mode = viewMode.value;
-  var a = document.createElement("a");
-  if (mode == "available") a.href = "/userDashboard";
-  else a.href = `/userDashboard/${mode}`;
-  a.click();
-});
